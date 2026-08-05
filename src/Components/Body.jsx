d4fdf5c01dc45d468b729e5c1d3e3c7dda31065a
KartikPanwar11 <kartikpanwar1101@gmail.com>
2026-08-05 23:16:39 +0530

    Enhance Body component: add sorting functionality and improve search layout

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,11 +2,24 @@ import Card from "./Card";
 import restaurantList from "../Data/res-list";
 
 const Body = ()=>{
+
+    let sortByRating = () => {
+        const filteredRestaurants = restaurantList.filter((res) => res.starRating >= 4.5);
+        console.log(filteredRestaurants);
+    }
+
+
     return (
         <div className="body">
             <div className="search">
                 <input placeholder="Search For Restaurants"></input>
-                <button>Search</button>
+                <div className="search-actions">
+                    <button type="button">Search</button>
+                    <button type="button" 
+                    className="sort-btn" 
+                    onClick={sortByRating}
+                    >Sort</button>
+                </div>
             </div>
             <div className="res-card">
                 {restaurantList.map((restaurant) => (
