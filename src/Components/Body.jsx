923e2d17014925c0cbedbcfb0e037ef0b0d62e9c
KartikPanwar11 <kartikpanwar1101@gmail.com>
2026-08-10 15:43:22 +0530

    Update fetch URL to use CORS proxy for restaurant data retrieval to bypass CORS Policy

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -17,7 +17,7 @@ const Body = () => {
     const fetchData = async () => {
         try {
             const data = await fetch(
-                "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9690247&lng=72.8205292&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+                "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9690247&lng=72.8205292&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
             );
 
             const json = await data.json();
