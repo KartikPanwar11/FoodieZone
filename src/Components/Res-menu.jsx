96072260ed1efd1097675750e062f3631ed4519e
KartikPanwar11 <kartikpanwar1101@gmail.com>
2026-08-16 17:35:07 +0530

    Trying menu fetching in RestaurantMenu component using useEffect

--- src/Components/Res-menu.jsx
+++ src/Components/Res-menu.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react";
+
 const RestaurantMenu = () => {
+
+    useEffect (()=>{
+        fetchMenu();
+    },[]);
+
+    const fetchMenu = async ()=>{
+        {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.9690247&lng=72.8205292&restaurantId=952722&catalog_qa=undefined&submitAction=ENTER"
+        );
+        const json = await data.json();
+        console.log(json);
+        }
+    };
+
+
+
   return (
     <div className="res-menu">
       <h1>Restaurant Name</h1>
